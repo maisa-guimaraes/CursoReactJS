436de53c25e3aac0662d607fe524aa7e44d583a6
Maísa Akiko Guimarães <127980915+maisa-guimaraes@users.noreply.github.com>
2025-11-26 11:15:37 -0300

    Add files via upload

---
+++ 02_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -0,0 +1,24 @@
+// components
+import FirstComponent from './components/FirstComponent'
+import TemplateExpressions from './components/TemplateExpressions'
+import MyComponent from './components/MyComponent'
+import Events from './components/Events'
+import Challenge from './components/Challenge'
+
+// Styles / CSS
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Fundamentos React</h1>
+      <FirstComponent />
+      <TemplateExpressions />
+      <MyComponent />
+      <Events />
+      <Challenge/>
+    </>
+  )
+}
+
+export default App
